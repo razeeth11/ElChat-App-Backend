d25106905f3760965fa855023f04e79042bb5207
razeeth11 <codebyrazeeth@gmail.com>
2026-01-03 19:10:53 +0530

    Removed-OTP-Providers

--- src/modules/auth/auth.services.js
+++ src/modules/auth/auth.services.js
@@ -1,25 +1,6 @@
-import twilio from "twilio";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+export async function sentOtp(user) {}
 
-export async function sentOtp(user) {
-  return await client.verify.v2
-    .services(process.env.TWILIO_SERVICE_SID)
-    .verifications.create({
-      channel: "sms",
-      to: user.phoneNumber,
-    });
-}
-
-export async function verifyOtp(user) {
-  return await client.verify.v2
-    .services(process.env.TWILIO_SERVICE_SID)
-    .verificationChecks.create({
-      code: user.otp,
-      to: user.phoneNumber,
-    });
-}
+export async function verifyOtp(user) {}
