62b6b737f69dd53f747df9c8d5d184a916718d5e
razeeth11 <codebyrazeeth@gmail.com>
2026-01-10 09:38:32 +0530

    Socket-Connection-Implemented

--- app.js
+++ app.js
@@ -6,15 +6,40 @@ import AuthRouter from "./src/modules/auth/auth.router.js";
 import ChatsRouter from "./src/modules/chats/chats.router.js";
 import UserRouter from "./src/modules/user/user.router.js";
 import ConversationsRouter from "./src/modules/conversations/conversations.router.js";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
-
 export const client = new MongoClient(MONGO_URL);
 await client.connect();
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+const onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("register-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-message", (userIds) => {
+    console.log(userIds);
+  });
+});
 
 app.use(cors());
 app.use(express.json());
@@ -27,7 +52,7 @@ app.get("/", (req, res) => {
   res.send("ElChat Backend is running");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
 });
 
